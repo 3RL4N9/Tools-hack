7743d957cfac9defd4cc76f476de9287abd070ef
Erlang <71025266+3RL4N9@users.noreply.github.com>
2020-09-10 15:58:14 +0700

    Add files via upload

---
+++ nothing.py
@@ -0,0 +1,166 @@
+#./bin/python
+
+import os,time
+os.system('clear')
+os.system('toilet -f standard Hay -F gay')
+time.sleep(2.00)
+os.system('clear')
+os.system('toilet -f standard Lope you -F gay')
+time.sleep(2.00)
+os.system('clear')
+
+
+def siapa():
+           nama = input('siapa nama lu?:')
+           time.sleep(2.00)
+           os.system('clear')
+           print("Hay",nama,"selamat datang di kumpulan tools Termux")
+           time.sleep(5.00)
+           os.system('clear')
+           time.sleep(2.00)
+           print("jangan lupa add Facebook gue")
+           time.sleep(2.00)
+           os.system('clear')
+siapa()
+
+for a in range(1, 1001):
+    time.sleep(0.01)
+    print(a, '1 3 4 7 5 3 8 4 8 3 8 4 8 5 8 6 8 5 7 5 4 6 8 6 7')
+def logo():print("""\33[4m
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeee     eeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeee             eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeee              eeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeee                    eeeeeeeeeeeeeeeeeeeeeee
+eeeeee                           eeeeeeeeeeeeeeeeeeeeee
+eeeee                                eeeeeeeeeeeeeeeeee
+eeeee                                 eeeeeeeeeeeeeeeee
+eeeee                                 eeeeeeeeeeeeeeeee
+eeeee                                 eeeeeeeeeeeeeeeee
+eeeee                                 eeeeeeeeeeeeeeeee
+eeeee                                 eeeeeeeeeeeeeeeee
+eeeeee                               eeeeeeeeeeeeeeeeee
+eeeeeee                             eeeeeeeeeeeeeeeeeee
+eeeeeeee                           eeeeeeeeeeeeeeeeeeee
+eeeeeeeee                         eeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeee                      eeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeee                    eeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+============User-Termux-Indonesia======================
+============Author=Erlang==============================
+============Facebook=Erlang Sans=======================
+============Github=https://github.com/3RL4N9===========
+""")
+logo()
+time.sleep(2.00)
+def menu():
+	print("""\33[35m
+{•}Menu
+{1}CrackFb
+
+{2}Tombol Termux
+
+{3}Ripper
+
+{4}DarkFb
+
+{5}HackIG
+
+{6}CrackFb (old)
+
+{7}InstaBruteIG
+
+{8}GmailHack
+
+{9}HackFb
+
+{A}Install Bahan
+{0}keluar
+""")
+menu()
+time.sleep(1.00)
+pilih = input("{?}root@system~#:  ")
+
+if pilih == '1':
+     time.sleep(2.00)
+     os.system('clear')
+     os.system('git clone https://github.com/RIZKY4/pro')
+     print("sukses")
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '2':
+     time.sleep(2.00)
+     os.system('clear')
+     os.system('pip2 install bkey')
+     print("sukses. ketik (bkey) sebanyak 2 x")
+if pilih == '3':
+     time.sleep(2.00)
+     os.system('clear')
+     os.system('git clone https://github.com/hekelpro/ripper')
+     print("sukses")
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '4':
+     os.system('clear')
+     os.system('git clone https://github.com/BOT-033/Sensei')
+     print("sukses")
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '5':
+     os.system('clear')
+     os.system('git clone https://github.com/kancotdq/igbrute')
+     print('sukses')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '6':
+     os.system('clear')
+     os.system('git clone https://github.com/RIZKY4/old')
+     print('sukses')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '7':
+     os.system('clear')
+     os.system('git clone https://github.com/Ha3MrX/InstaBrute')
+     print('sukses')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '8':
+     os.system('clear')
+     os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
+     print('sukses')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '9':
+     os.system('clear')
+     os.system('git clone https://github.com/haniefaktivisme/hackfb.git')
+     print('sukses')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == 'A':
+     time.sleep(2.00)
+     os.system('''
+pkg update && upgrade
+pkg install git python python2 php nano ruby
+pip install requests mechanize bs4 tqdm
+pip2 install requests mechanize bs4 tqdm
+pkg install toilet figlet && gem install lolcat
+''')
+     time.sleep(2.00)
+     os.system('python nothing.py')
+if pilih == '0':
+     time.sleep(2.00)
+     os.system('toilet -f standard thanks -F gay')
+     time.sleep(2.00)
+     os.system('toilet -f standard see you -F gay')
+else:
+    print("Salah Boss. Ketik yang benar")
+    time.sleep(2.00)
+    os.system('python nothing.py')
+
