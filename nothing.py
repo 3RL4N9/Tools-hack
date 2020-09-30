a931dacf5bf7cde305446b2872b65fb8b1e81e9d
Erlang <71025266+3RL4N9@users.noreply.github.com>
2020-09-30 15:53:16 +0700

    NothingV2.py

--- nothing.py
+++ nothing.py
@@ -1,7 +1,4 @@
 #./bin/python
-#mau recode??
-#masak iya tools kayak gini mau di recode
-
 import os,time
 os.system('clear')
 os.system('toilet -f standard Hay -F gay')
@@ -13,14 +10,14 @@ os.system('clear')
 
 
 def siapa():
-           nama = input('siapa nama lu?:')
+           nama = input('siapa nama kamu?:')
            time.sleep(2.00)
            os.system('clear')
            print("Hay",nama,"selamat datang di kumpulan tools Termux")
            time.sleep(5.00)
            os.system('clear')
            time.sleep(2.00)
-           print("jangan lupa add Facebook gue")
+           print("jangan lupa add Facebook aku")
            time.sleep(2.00)
            os.system('clear')
 siapa()
@@ -29,7 +26,7 @@ for a in range(1, 1001):
     time.sleep(0.01)
     print(a, '1 3 4 7 5 3 8 4 8 3 8 4 8 5 8 6 8 5 7 5 4 6 8 6 7')
     os.system('clear')
-def logo():print("""\33[4m
+def logo():print("""\33[32m
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeee     eeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeee       eeeeeeeeeeeeeeeeeeeeeeeeeeee
@@ -85,6 +82,7 @@ def menu():
 {9}HackFb
 
 {A}Install Bahan
+
 {0}keluar
 """)
 menu()
@@ -96,8 +94,8 @@ if pilih == '1':
      os.system('clear')
      os.system('git clone https://github.com/RIZKY4/pro')
      print("sukses")
+     sys.exit()
      time.sleep(2.00)
-     os.system('python nothing.py')
 if pilih == '2':
      time.sleep(2.00)
      os.system('clear')
@@ -109,43 +107,43 @@ if pilih == '3':
      os.system('git clone https://github.com/hekelpro/ripper')
      print("sukses")
      time.sleep(2.00)
-     os.system('python nothing.py')
+     sys.exit()
 if pilih == '4':
      os.system('clear')
      os.system('git clone https://github.com/BOT-033/Sensei')
      print("sukses")
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == '5':
      os.system('clear')
      os.system('git clone https://github.com/kancotdq/igbrute')
      print('sukses')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == '6':
      os.system('clear')
      os.system('git clone https://github.com/RIZKY4/old')
      print('sukses')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == '7':
      os.system('clear')
      os.system('git clone https://github.com/Ha3MrX/InstaBrute')
      print('sukses')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == '8':
      os.system('clear')
      os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
      print('sukses')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == '9':
      os.system('clear')
      os.system('git clone https://github.com/haniefaktivisme/hackfb.git')
      print('sukses')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     os.system('ls')
 if pilih == 'A':
      time.sleep(2.00)
      os.system('''
@@ -156,7 +154,7 @@ pip2 install requests mechanize bs4 tqdm
 pkg install toilet figlet && gem install lolcat
 ''')
      time.sleep(2.00)
-     os.system('python nothing.py')
+     sys.exit()
 if pilih == '0':
      time.sleep(2.00)
      os.system('toilet -f standard thanks -F gay')
